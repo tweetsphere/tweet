52bd5bd5f0391cf7d8aadd2a768c02d6cd7000e0
Chinthaka A <chinthaka@ceynet.asia>
2015-07-19 10:20:34 +0530

    changed style/dashboard

--- index.php
+++ index.php
@@ -1,87 +1,58 @@
 <?php
-session_start();
+date_default_timezone_set('Asia/Colombo');
 error_reporting(E_ALL);
-ini_set('display_errors', 'yes');
-
+ini_set('display_errors', "no");
 require("config.php");
-require 'libs/Validate.class.php';
-require 'libs/DBAccess.php';
-require 'libs/Category.class.php';
-require 'libs/Order.class.php';
-require 'libs/Customer.class.php';
-require 'libs/Product.class.php';
-require 'libs/Common.class.php';
-require 'libs/Supplier.class.php';
-require_once 'libs/ExcelRead.class.php';
-    
- if(isset($_GET['logout'])== true){
-    unset($_SESSION['userId']);
- }
+require("twitteroauth/twitteroauth.php");
+//require_once "/var/www/html/phpInsight-master/autoload.php";
+require_once "libs/phpInsight-master/autoload.php";
+$sentiment = new \PHPInsight\Sentiment();
 ?>
 
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Dashboard - Rfabric Admin</title>
-<link href="styles/style.css" rel="stylesheet" type="text/css" />
-<script>
-function myFunction() {
-    window.print();
-}
-</script>
+<meta charset="utf-8" />
+<title>Tweet Tweet</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<link href="css/twstyles.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 </head>
 <body>
-<div id="Wrap">
-<div id="header"><a href="index.php" id="logo-main"><img src="images/rfabric-logo-main.png" title="Rfabric" alt="Rfabric Logo" border="0" /></a>
+<div id="TweetTweet">
+	<header><div id="Header">
+    	<hgroup><a href="#" class="Logo">tweet</a><h1><a href="#">Tweetspher</a></h1>	
+        <h2>Admin</h2></hgroup>
+        <nav><ul id="Navi">
+        <li><a href="index.php?page=tweets">View Tweets</a></li>
+        <li><a href="index.php?page=scrap">Scrap Data</a></li>
+        <li><a href="index.php?page=showdata">Sentiment</a></li>
+		<li><a href="index.php?page=users">Users</a></li>
+        </ul></nav>
+<?php
+if (!$link = mysql_connect(_DB_SERVER_, _DB_USER_, _DB_PASSWORD_)) {
+    echo 'Could not connect to mysql';
+    exit;
+}
 
-    
- <?php if(isset($_SESSION['userId']) && $_SESSION['userId']!=""){ ?> 
-<div id="loginusr">Welcome <b>Admin</b>, <a href="index.php?page=login&logout=true">Log out</a></div>
-</div>
-<div id="sidebar">
-<div class="sidetitle">Category</div>
-<div class="sidetitlesub"><a href="index.php?page=categories">Categories</a></div>
-<div class="sidetitlesub"><a href="index.php?page=add_category">Add Category</a></div>
-
-<div class="sidetitle">Vendor</div>
-<div class="sidetitlesub"><a href="#">Vendors</a></div>
-<div class="sidetitlesub"><a href="#">Add New Vendor</a></div>
-
-<div class="sidetitle">Product</div>
-<div class="sidetitlesub"><a href="index.php?page=products">Products</a></div>
-<div class="sidetitlesub"><a href="index.php?page=add_product">Add Product</a></div>
-
-
-<div class="sidetitle">Order Tracking</div>
-<div class="sidetitlesub"><a href="index.php?page=transactions">Transactions/Orders</a></div>
-
-<div class="sidetitle">Reports</div>
-<div class="sidetitlesub"><a href="#">Daily Transaction</a></div>
-<div class="sidetitlesub"><a href="#">Customers</a></div>
-</div>
-     
-<?php }?>
-<div id="bodymain">
-<?php 
-        if(isset($_GET['page'])){                       
-                $includePage = "includes/".$_GET['page'].".php";                     
-        }
-        else{
-            $includePage = "includes/login.php";
-        }
-        if(isset($includePage)){
+if (!mysql_select_db(_DB_NAME_, $link)) {
+    echo 'Could not select database';
+    exit;
+}
+            if (isset($_GET['page'])) {
+                $includePage = "includes/" . $_GET['page'] . ".php";
+            } else {
+                $includePage = "includes/tweets.php";
+            }
+            if (isset($includePage)) {
                 include($includePage);
-            }      
-      ?>
-
-</div>
-
-</div>
-<div id="footermain"><div>
-<p style="float:left;"><a href="#">Documentation</a> | <a href="#">Support</a></p>
-<span id="footerinner">Powered by : <a href="#">Powered By SLIIT WE Group 41</a></span></div>
-</div>
+            }
+?>
+<footer>
+<p>Copyright &copy; 2015 SLIIT WE Group 50</p>
+</footer>     
+</div><!--TweetTweet-->
 </body>
 </html>
